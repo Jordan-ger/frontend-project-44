f18b62ba461b42ecbefa1f68efa940394068c2fa
Jordan-ger <kalsn.ger@mail.ru>
2024-02-09 11:17:13 +0300

    add

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/hellatriggeropp/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/hellatriggeropp/frontend-project-44/actions)
-  [![asciinema]] (https://asciinema.org/connect/cd72a2fa-ef62-4ae0-accd-c41d291d9fc3)
+![Asciinema Calculator][https://asciinema.org/connect/cd72a2fa-ef62-4ae0-accd-c41d291d9fc3]
+
+![Asciinema even numbers, odd][https://asciinema.org/a/lB6aNrqGakBohygnIPE8r4khF]
+
+![Asciinema salutation][https://asciinema.org/a/Jpy3iCWaW0LTEsJQiZihsCc7L]
--- src/games/even.js
+++ src/games/even.js
@@ -1,35 +1,20 @@
-import readlineSync from 'readline-sync';
+#!/usr/bin/env node
+import { getRandomNumber } from '../utils.js';
+import run from '../index.js';
 
-const isEven = (num) => num % 2 === 0;
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const minRange = 0;
+const maxRange = 100;
 
-const playEvenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  
-  let correctAnswersCount = 0;
-  
-  while (correctAnswersCount < 3) {
-    const randomNumber = Math.floor(Math.random() * 100);
-    console.log(`Question: ${randomNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    
-    const isCorrectAnswer = (isEven(randomNumber) && userAnswer === 'yes') ||
-                            (!isEven(randomNumber) && userAnswer === 'no');
-    
-    if (isCorrectAnswer) {
-      console.log('Correct!');
-      correctAnswersCount += 1;
-    } else {
-      const correctAnswer = isEven(randomNumber) ? 'no' : 'yes';
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
-  
-  console.log(`Congratulations, ${name}!`);
+const isEven = (number) => (number % 2 === 0);
+
+const generateRound = () => {
+  const number = getRandomNumber(minRange, maxRange);
+  const question = number.toString();
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
 
-export default playEvenGame;
+export default () => {
+  run(description, generateRound);
+};
